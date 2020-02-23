0ad65da781ca3d18baa1845404dba3395e2749eb
Ubuntu <ubuntu@ip-172-31-85-193.ec2.internal>
2020-02-23 04:23:46 +0000

    installed socketio

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
-import flask, flask_sqlalchemy
+import flask, flask_sqlalchemy, flask_socketio
 
 app = flask.Flask(__name__)
+
+socketio = flask_socketio.SocketIO(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<gMakunde>:<San20Faith>@localhost/postgres'  
 db = flask_sqlalchemy.SQLAlchemy(app)
@@ -10,7 +12,12 @@ db = flask_sqlalchemy.SQLAlchemy(app)
 def hello():
     return flask.render_template('index.html')
 
-app.run(
+@socketio.on('connect')
+def on_connect():
+    print('someone connected!')
+
+socketio.run(
+    app,
     host=os.getenv('IP', '0.0.0.0'),
     port=int(os.getenv('PORT', 8080)),
     debug=True
