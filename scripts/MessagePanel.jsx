e406a5904f34254c9ea0c81ed1655288af9e0334
Ubuntu <ubuntu@ip-172-31-85-193.ec2.internal>
2020-02-23 14:54:41 +0000

    moving button into new file

--- scripts/MessagePanel.jsx
+++ scripts/MessagePanel.jsx
@@ -1,40 +1,7 @@
 import * as React from 'react';
+import { Socket } from './Socket'
 
-const User = {
-	username: '',
-	profilePic: false,
-	bot: false
-}
 
-const Message = {
-	user: User,
-	msg: ''
-}
-
-function InputUsername(){
-	return (
-		<div className="input-group" style={{paddingBottom:'5px'}}>
-			<div className="input-group-append">
-				<span className="input-group-text attach_btn"><i className="fas fa-paperclip"></i></span>
-			</div>
-			<textarea name="" id="username" className="form-control type_msg_sm" placeholder="Enter your username"></textarea>
-		</div>
-		);
-}
-
-function InputMessage(){
-	return(
-		<div className="input-group">
-				<div className="input-group-append">
-					<span className="input-group-text attach_btn"><i className="fas fa-paperclip"></i></span>
-				</div>
-				<textarea name="" id="message" className="form-control type_msg" placeholder="Type your message..."></textarea>
-				<div className="input-group-append" id="send">
-					<span className="input-group-text send_btn"><i className="fas fa-location-arrow"></i></span>
-				</div>
-			</div>
-		);
-}
 
 function ProfilePicture(props){
 	if(props.bot) {
@@ -95,16 +62,7 @@ function Response(props) {
 
 export class MessagePanel extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-			messages: [],
-        }
-    };
-    
-    sendUserMessage(){
-    	
-    	this.setState({
-    	});
+        super();
     }
     
     render() {
