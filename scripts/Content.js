a68077395101dd3c0534fb44e1a82b7303500719
Ubuntu <ubuntu@ip-172-31-85-193.ec2.internal>
2020-02-23 00:42:33 +0000

    seperated each component into a seperate variable

--- scripts/Content.js
+++ scripts/Content.js
@@ -1,16 +1,28 @@
     
 import * as React from 'react';
 
-const htmlBody = <div className="container-fluid h-100">
-            <h1 style={{textAlign:"center", color:"white", paddingBottom:"20px", paddingTop:"20px"}}> Chat man, chat man, yea thats me! </h1>
-			<div className="row justify-content-center h-100">
-				<div className="col-md-4 col-xl-3 chat"><div className="card mb-sm-3 mb-md-0 contacts_card">
-					<div className="caheader">
-						
-					</div>
-					<div className="card-body contacts_body">
-						<ui className="contacts">
-						<li className="active">
+const messageSend = <div className="d-flex justify-content-start mb-4">
+								<div className="img_cont_msg">
+									<img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png" className="rounded-circle user_img_msg"/>
+								</div>
+								<div className="msg_cotainer">
+									George is Awesome!
+									<span className="msg_time">Username</span>
+								</div>
+							</div>;
+							
+const botResponse = <div className="d-flex justify-content-start mb-4">
+								<div className="img_cont_msg">
+									<img src="https://www.cartoonbucket.com/wp-content/uploads/2017/04/Jenny-Wakeman-Smiling.png" className="rounded-circle user_img_msg"/>
+								</div>
+								<div style={{paddingRight:'10px'}}></div>
+								<div className="msg_cotainer_send">
+									Beep boop bop beep
+									<span className="msg_time">Teenage Robot</span>
+								</div>
+							</div>;
+							
+const usersOnline = <li className="active">
 							<div className="d-flex bd-highlight">
 								<div className="img_cont">
 									<img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png" className="rounded-circle user_img"/>
@@ -21,52 +33,15 @@ const htmlBody = <div className="container-fluid h-100">
 									<p>Username is online</p>
 								</div>
 							</div>
-						</li>
-						</ui>
-					</div>
-					<div className="card-footer"></div>
-				</div></div>
-				<div className="col-md-8 col-xl-6 chat">
-					<div className="card">
-						<div className="card-header msg_head">
-							<div className="d-flex bd-highlight">
-								<div className="user_info">
-									<span>Chat with Username</span>
-									<p>1767 Messages</p>
-								</div>
-								
-							</div>
-							
-						</div>
-						<div className="card-body msg_card_body">
-							<div className="d-flex justify-content-start mb-4">
-								<div className="img_cont_msg">
-									<img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png" className="rounded-circle user_img_msg"/>
-								</div>
-								<div className="msg_cotainer">
-									George is Awesome!
-									<span className="msg_time">Username</span>
-								</div>
-							</div>
-							<div className="d-flex justify-content-start mb-4">
-								<div className="img_cont_msg">
-									<img src="https://www.cartoonbucket.com/wp-content/uploads/2017/04/Jenny-Wakeman-Smiling.png" className="rounded-circle user_img_msg"/>
-								</div>
-								<div style={{paddingRight:'10px'}}></div>
-								<div className="msg_cotainer_send">
-									Beep boop bop beep
-									<span className="msg_time">Teenage Robot</span>
-								</div>
-							</div>
-						</div>
-						<div className="card-footer">
-							<div className="input-group" >
+						</li>;
+
+const messageInput = < div>
+							<div className="input-group" style={{paddingBottom:'5px'}}>
 								<div className="input-group-append">
 									<span className="input-group-text attach_btn"><i className="fas fa-paperclip"></i></span>
 								</div>
 								<textarea name="" className="form-control type_msg_sm" placeholder="Enter your username"></textarea>
 							</div>
-							<div style={{paddingBottom:'10px'}}></div>
 							<div className="input-group">
 								<div className="input-group-append">
 									<span className="input-group-text attach_btn"><i className="fas fa-paperclip"></i></span>
@@ -76,6 +51,44 @@ const htmlBody = <div className="container-fluid h-100">
 									<span className="input-group-text send_btn"><i className="fas fa-location-arrow"></i></span>
 								</div>
 							</div>
+						</div>;
+
+const userCount = <span> 1 Person online </span>
+
+const chatWith = <span>Chat with Username</span>
+
+const messageCount = <p>1767 Messages</p>
+
+const htmlBody = <div className="container-fluid h-100">
+            <h1 style={{textAlign:"center", color:"white", paddingBottom:"20px", paddingTop:"20px"}}> Chat man, chat man, yea thats me! </h1>
+			<div className="row justify-content-center h-100">
+				<div className="col-md-4 col-xl-3 chat"><div className="card mb-sm-3 mb-md-0 contacts_card">
+					<div className="caheader user_info">
+						
+					</div>
+					<div className="card-body contacts_body">
+						<ui className="contacts">
+
+						</ui>
+					</div>
+					<div className="card-footer"></div>
+				</div></div>
+				<div className="col-md-8 col-xl-6 chat">
+					<div className="card">
+						<div className="card-header msg_head">
+							<div className="d-flex bd-highlight">
+								<div className="user_info">
+									
+								</div>
+								
+							</div>
+							
+						</div>
+						<div className="card-body msg_card_body">
+							
+						</div>
+						<div className="card-footer">
+							
 						</div>
 					</div>
 				</div>
@@ -83,6 +96,14 @@ const htmlBody = <div className="container-fluid h-100">
 		</div>;
 
 export class Content extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			usersList: [],
+			messages: [],
+			
+		}
+	};
     render() {
         return htmlBody;
         
