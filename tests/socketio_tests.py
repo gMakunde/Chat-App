fb4ce79ff9a7211eafe4f207089418db788112e6
Ubuntu <ubuntu@ip-172-31-85-193.ec2.internal>
2020-03-16 19:02:20 +0000

    testing if server emits new message

--- tests/socketio_tests.py
+++ tests/socketio_tests.py
@@ -3,7 +3,7 @@ import app
 import models
 
 class SocketIOTestCase(unittest.TestCase):
-    def test_server_relays_message(self):
+    def test_on_connect(self):
         client = app.socketio.test_client(app.app)
         response = client.get_received()
         
@@ -28,6 +28,33 @@ class SocketIOTestCase(unittest.TestCase):
             })
         data = from_server["args"][0]
         self.assertEqual(data["messages"], message_list)
+    
+    def test_new_message(self):
+        client = app.socketio.test_client(app.app)
+        msg = {
+            'user': {
+                'username': 'fred', 
+                'profilePic': None, 
+                'bot': False
+            },
+            'msg': 'george is so beautiful'
+        }
+        client.emit("new message", {
+            "user_message": msg
+        })
+        response = client.get_received()
+
+        self.assertEqual(len(response), 2)
+        from_server = response[1]
+        self.assertEqual(
+            from_server["name"],
+            "message received"
+        )
+        data = from_server["args"][0]
+        self.assertEqual(
+            data["message"],
+            msg
+        )
             
 if __name__ == '__main__':
     unittest.main()
