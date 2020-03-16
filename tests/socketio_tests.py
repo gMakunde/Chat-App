22a0045757f3013460e7938b93adc1d270ada9ef
Ubuntu <ubuntu@ip-172-31-85-193.ec2.internal>
2020-03-16 15:56:26 +0000

    uncommented code to wee diference

--- tests/socketio_tests.py
+++ tests/socketio_tests.py
@@ -1,20 +1,19 @@
 import unittest
-#import app, os, flask_socketio
+import app, os, flask_socketio
 
 class SocketIOTestCase(unittest.TestCase):
     def test_server_relays_message(self):
-        # client = app.socketio.test_client(app.app)
-        # response = client.get_received()
+        client = app.socketio.test_client(app.app)
+        response = client.get_received()
         
-        # self.assertEqual(len(response), 1)
-        # from_server = response[0]
-        # self.assertEqual(
-        # from_server["name"], 
-        # "message_list received"
-        # )
-        # data = from_server["args"][0]
-        # self.assertEqual(data["messages"], "")
-        self.assertEqual("", "")
+        self.assertEqual(len(response), 1)
+        from_server = response[0]
+        self.assertEqual(
+        from_server["name"], 
+        "message_list received"
+        )
+        data = from_server["args"][0]
+        self.assertEqual(data["messages"], "")
             
 if __name__ == '__main__':
     unittest.main()
